24f4be649944fa7897a3f3dc312b89d3a7b61ac9
maryhands <maryhands915@gmail.com>
2021-07-11 01:10:50 +0900

    로그인 페이지 초안 제작/ 2021-07-11/ 작성자 : 신수원

--- FinalProject/app/src/main/java/com/erp/project/SplashActivity.kt
+++ FinalProject/app/src/main/java/com/erp/project/SplashActivity.kt
@@ -1,11 +1,24 @@
 package com.erp.project
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.erp.project.login.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val test = Intent(this, LoginActivity::class.java)
+
+        try{
+            Thread.sleep(3000)
+        }
+        catch(e:Exception){
+
+        }
+        startActivity(test)
+        finish()
     }
 }
