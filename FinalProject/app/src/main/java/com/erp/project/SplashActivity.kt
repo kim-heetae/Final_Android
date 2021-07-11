a15ff315306cfe66c3a295cd227c4270a5b1e0f2
wpwjd169 <wpwjd169@naver.com>
2021-07-11 14:28:17 +0900

    shin pull 받기용 commit / 21.07.11 14:27 / 작성자 : 박제정

--- FinalProject/app/src/main/java/com/erp/project/SplashActivity.kt
+++ FinalProject/app/src/main/java/com/erp/project/SplashActivity.kt
@@ -1,11 +1,23 @@
 package com.erp.project
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.erp.project.login.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val nextLoginIntent = Intent(this, LoginActivity::class.java)
+        try {
+            Thread.sleep(3000)
+        }catch (e : Exception){
+            Log.i("LoginActivity", e.toString())
+        }
+        startActivity(nextLoginIntent)
+        finish()
     }
 }
